3fb17ada4bb638c98bf9e485b66903d65f0ac86c
Sempai-07 <mausm471@gmail.com>
2025-01-28 16:27:39 +0200

    Delete(Interpreter): process events

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -76,12 +76,6 @@ class Interpreter {
     this.globalScore.create("#exports", {});
 
     this.globalScore.create("#options", options.options);
-
-    process.on("unhandledRejection", (reason) => {
-      console.error(
-        `This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "${reason}".`,
-      );
-    });
   }
 
   run() {
