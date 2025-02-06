b65ba24eda9ef7c416d3a7eccde3eefb1b20adc6
Sempai-07 <mausm471@gmail.com>
2025-02-06 16:32:22 +0200

    Update(ReturnStatement): error logger

--- src/ast/statement/ReturnStatement.ts
+++ src/ast/statement/ReturnStatement.ts
@@ -1,7 +1,6 @@
 import { StmtType } from "../StmtType";
 import { type Position } from "../../lexer/Position";
 import { Environment } from "../../Environment";
-import { BaseError } from "../../errors/BaseError";
 
 class ReturnStatement extends StmtType {
   public readonly body: StmtType | StmtType[];
@@ -16,27 +15,13 @@ class ReturnStatement extends StmtType {
   }
 
   evaluate(score: Environment) {
-    try {
-      if (Array.isArray(this.body)) {
-        return this.body.map((value) => value.evaluate(score));
-      }
-      if (!(this.body instanceof StmtType)) {
-        return this.body;
-      }
-      return this.body.evaluate(score);
-    } catch (err) {
-      if (err instanceof BaseError) {
-        err.files = Array.from(
-          new Set([score.get("import").main, ...err.files]),
-        ).map((file) => {
-          if (file === score.get("import").main) {
-            return `${file}:${this.position.line}:${this.position.column}`;
-          }
-          return file;
-        });
-      }
-      throw err;
+    if (Array.isArray(this.body)) {
+      return this.body.map((value) => value.evaluate(score));
     }
+    if (!(this.body instanceof StmtType)) {
+      return this.body;
+    }
+    return this.body.evaluate(score);
   }
 }
 
